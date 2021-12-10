1a2bbacb6c7fb038ccba9e877cc3672f3fc815ab
va <anohin.va@gmail.com>
2021-12-10 18:10:59 +0200

    Init lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spark-in-action-using-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/spark_in_action_using_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns spark-in-action-using-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [spark-in-action-using-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
